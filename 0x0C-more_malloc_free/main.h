374372ce20785bcef4935c02227ca9bc36ab38be
faty2018 <layfabff@gmail.com>
2023-07-13 17:45:05 -0700

    commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,20 +1,14 @@
-#include <stdlib.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: a pointer to the allocated memory
- */
-void *malloc_checked(unsigned int b)
-{
-	void *ptr;
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
-		exit(98);
-
-	return (ptr);
-}
+#endif
